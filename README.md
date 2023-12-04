a007cf22afe902d0aa30db6c0b0752a98583195b
feelonewong <feelonewong@gmail.com>
2023-12-04 23:04:15 +0800

    update: 配置文件的读取

---
+++ README.md
@@ -0,0 +1,11 @@
+* config 配置文件
+* api 接口相关文件
+* core
+* flag
+* global 全局文件
+* middleware 中间件
+* models 数据模型
+* routers 路由文件
+* service 服务方法
+* utils 工具函数
+* main.go 服务启动
---
+++ core/conf.go
@@ -0,0 +1,25 @@
+package core
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"gvb_server/config"
+	"gvb_server/global"
+	"io/ioutil"
+	"log"
+)
+
+func InitConf() {
+	const ConfigFile = "settings.yaml"
+	c := &config.Config{}
+	yamlConf, err := ioutil.ReadFile(ConfigFile)
+	if err != nil {
+		panic(fmt.Errorf("get yamlConf error:%s", err))
+	}
+	err = yaml.Unmarshal(yamlConf, c)
+	if err != nil {
+		log.Fatalf("config Init Unmarshal: %v", err)
+	}
+	log.Println("config yamlFile load Init success.")
+	global.Config = c
+}
