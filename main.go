60700e81ba1c1f7de162f29dc54eba217370b84e
feelonewong <feelonewong@gmail.com>
2023-12-07 00:28:05 +0800

    update: 05_自定义logger

--- main.go
+++ main.go
@@ -8,5 +8,11 @@ import (
 func main() {
 	// 读取配置文件
 	core.InitConf()
+	// 初始化log
+	global.Log = core.InitLogger()
+	global.Log.Info("info 提示")
+	global.Log.Warnln("嘻嘻嘻哈哈哈")
+	global.Log.Error("鱻鱼鱼笑脸瞎")
+	// 初始化数据库
 	global.DB = core.InitGorm()
 }
