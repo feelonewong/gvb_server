4cc49ce5ef60a50d57ece3cd893578881b80f6db
feelonewong <feelonewong@gmail.com>
2023-12-08 17:42:24 +0800

    update: 06_初始化路由

--- router/entry.go
+++ router/entry.go
@@ -1,8 +1,13 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"gvb_server/global"
+
+	"github.com/gin-gonic/gin"
+)
 
 func InitRouter() *gin.Engine {
+	gin.SetMode(global.Config.System.Env)
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
